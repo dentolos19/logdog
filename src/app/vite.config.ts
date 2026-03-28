3e32d94e4ef546c48b422333f42ef7032184df1c
Dennise Catolos <dentolos19@gmail.com>
2026-03-28 13:35:09 +0800

    Initial revamp

---
+++ src/app/vite.config.ts
@@ -0,0 +1,25 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tanstackStart(),
+    viteReact(),
+    tailwindcss(),
+    devtools(),
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
+  ],
+});
+
+export default config;
